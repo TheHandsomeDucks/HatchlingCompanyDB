ae075dd31793f1bdd5d15272be7f41de5485c468
mkovachev <martin.kovachev@outlook.com>
2018-03-15 21:03:49 +0200

    added IHatchlingCompanyDbContext

--- HatchlingCompany/HatchlingCompany.Data/HatchlingCompanyDbContext.cs
+++ HatchlingCompany/HatchlingCompany.Data/HatchlingCompanyDbContext.cs
@@ -6,13 +6,24 @@ using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace HatchlingCompany.Data
 {
-    public class HatchlingCompanyDbContext : DbContext
+    public class HatchlingCompanyDbContext : DbContext, IHatchlingCompanyDbContext
     {
         public HatchlingCompanyDbContext() : base("HatchlingCompanyConnection")
         {
             var strategy = new MigrateDatabaseToLatestVersion<HatchlingCompanyDbContext, Configuration>();
             Database.SetInitializer(strategy);
         }
+
+        public virtual IDbSet<Employee> Employees { get; set; }
+
+        public virtual IDbSet<EmployeeDetail> EmployeeDetails { get; set; }
+
+        public virtual IDbSet<Department> Departments { get; set; }
+
+        public virtual IDbSet<Town> Towns { get; set; }
+
+        public virtual IDbSet<Country> Countries { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -21,16 +32,6 @@ namespace HatchlingCompany.Data
 
             base.OnModelCreating(modelBuilder);
         }
-
-        public IDbSet<Employee> Employees { get; set; }
-
-        public IDbSet<EmployeeDetail> EmployeeDetails { get; set; }
-
-        public IDbSet<Department> Departments { get; set; }
-
-        public IDbSet<Town> Towns { get; set; }
-
-        public IDbSet<Country> Countries { get; set; }
 
     }
 }
---
+++ HatchlingCompany/HatchlingCompany.Data/IHatchlingCompanyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HatchlingCompany.Models;
+using System.Data.Entity;
+
+namespace HatchlingCompany.Data
+{
+    public interface IHatchlingCompanyDbContext
+    {
+
+        IDbSet<Employee> Employees { get; set; }
+
+        IDbSet<EmployeeDetail> EmployeeDetails { get; set; }
+
+        IDbSet<Department> Departments { get; set; }
+
+        IDbSet<Town> Towns { get; set; }
+
+        IDbSet<Country> Countries { get; set; }
+    }
+}

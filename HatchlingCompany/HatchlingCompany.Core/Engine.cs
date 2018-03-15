0f94ed9cec1d55f21b838b007782646fbcc2eecd
IovoslavIovchev <iovoslav.i@gmail.com>
2018-03-15 11:00:52 +0200

    Added Utils & Core Assemblies; Created Engine skeleton

---
+++ HatchlingCompany/HatchlingCompany.Core/Engine.cs
@@ -0,0 +1,17 @@
+﻿using HatchlingCompany.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatchlingCompany.Core
+{
+    public class Engine : IEngine
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

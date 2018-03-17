f42a075038fbe687e7aab536857a69f4661601a3
mkovachev <martin.kovachev@outlook.com>
2018-03-17 11:23:19 +0200

    added nav to Command folder

--- HatchlingCompany/HatchlingCompany.Core/Commands/Implementations/HelpCommand.cs
+++ HatchlingCompany/HatchlingCompany.Core/Commands/Implementations/HelpCommand.cs
@@ -37,6 +37,7 @@ namespace HatchlingCompany.Core.Commands.Implementations
         private IEnumerable<string> GetAllCommandNames()
         {
             var assembly = Assembly.GetAssembly(typeof(ICommand)); /// TODO get Commands folder only
+                // .Where(x => x.Namespace.Contains("Commands")); 
             var types = assembly.DefinedTypes
                 .Where(type => type.ImplementedInterfaces.Any(i => i == typeof(ICommand))).ToList();
 

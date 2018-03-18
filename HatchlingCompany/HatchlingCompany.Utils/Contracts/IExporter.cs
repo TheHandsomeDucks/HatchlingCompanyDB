7832c5dee5935f6334f3e291da7fca33430e79c7
IovoslavIovchev <iovoslav.i@gmail.com>
2018-03-18 14:59:19 +0200

    Added JSONExporter class and IExporter interface

---
+++ HatchlingCompany/HatchlingCompany.Utils/Contracts/IExporter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatchlingCompany.Utils.Contracts
+{
+    public interface IExporter
+    {
+        void Export(object obj);
+
+        //void Export(object obj, string path);
+    }
+}
---
+++ HatchlingCompany/HatchlingCompany.Utils/JSONExporter.cs
@@ -0,0 +1,47 @@
+﻿using HatchlingCompany.Utils.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatchlingCompany.Utils
+{
+    public class JSONExporter : IExporter
+    {
+        private string path;
+        private string fileName;
+
+        public JSONExporter()
+        {
+            this.path = "./../../../JSON/Exports/";
+            this.fileName = "export.json";
+        }
+
+        public void Export(object obj)
+        {
+            string serialized = JsonConvert.SerializeObject(obj);
+
+            SaveToFile(serialized, this.path, this.fileName);
+        }
+
+        public void Export(object obj, string path, string fileName)
+        {
+            string serialized = JsonConvert.SerializeObject(obj);
+
+            SaveToFile(serialized, path, fileName);
+        }
+
+        private void SaveToFile(string text, string path, string fileName)
+        {
+            string file = String.Concat(path, fileName);
+
+            using (StreamWriter writer = File.CreateText(file))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+}

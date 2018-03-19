96eeff51face71f81d90805eb115adad101de454
orlindraganov <orlindraganov@gmail.com>
2018-03-19 08:55:33 +0200

    Started working on comments

---
+++ HatchlingCompany/HatchlingCompany.Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatchlingCompany.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int AuthorId { get; set; }
+        public virtual Employee Author { get; set; }
+
+        public int RecipientId { get; set; }
+        public virtual Employee Recipient { get; set; }
+
+        public string Content { get; set; }
+    }
+}

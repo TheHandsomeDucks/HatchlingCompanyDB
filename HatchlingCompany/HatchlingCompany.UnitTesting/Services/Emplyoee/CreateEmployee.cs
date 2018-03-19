07b3040e191df82140731934a4430bb326a9264a
mkovachev <martin.kovachev@outlook.com>
2018-03-19 19:44:08 +0200

    added unit test layer

---
+++ HatchlingCompany/HatchlingCompany.UnitTesting/Services/Emplyoee/CreateEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HatchlingCompany.UnitTesting.Services.Emplyoee
+{
+    [TestClass]
+    public class CreateEmployee
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

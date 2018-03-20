13b3bafa93dbab502fd2debd5b7553813f0b8cdd
mkovachev <martin.kovachev@outlook.com>
2018-03-20 11:45:57 +0200

    createEmployee tested

--- HatchlingCompany/HatchlingCompany.UnitTesting/Services/Emplyoee/CreateEmployeeTests.cs
+++ HatchlingCompany/HatchlingCompany.UnitTesting/Services/Emplyoee/CreateEmployeeTests.cs
@@ -2,6 +2,7 @@
 using HatchlingCompany.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 
 namespace HatchlingCompany.UnitTesting.Services.Emplyoee
@@ -9,6 +10,23 @@ namespace HatchlingCompany.UnitTesting.Services.Emplyoee
     [TestClass]
     public class CreateEmployeeTests
     {
+        [TestMethod]
+        public void CreateEmployee_Should_Throw_DbEntityValidationException_If_Email_Is_Null()
+        {
+            // Arragne
+            var dbMock = new HatchlingCompanyDbContext(Effort.DbConnectionFactory.CreateTransient());
+            var createEmployeeService = new CreateEmployee(dbMock);
+
+            //Act
+            var parameters = new List<string>()
+            {
+                "createEmployee", "Alex", "Alexov", null, "phone"
+            };
+
+            // Assert
+            Assert.ThrowsException<DbEntityValidationException>(() => createEmployeeService.Execute(parameters));
+        }
+
         [TestMethod]
         public void CreateEmployee_Should_Create_Employee()
         {
@@ -19,14 +37,14 @@ namespace HatchlingCompany.UnitTesting.Services.Emplyoee
             //Act
             var returnMessage = createEmployeeService.Execute(new List<string>()
             {
-                "createEmployee", "Alex", "Alexov", "maila", "phone"
+                "createEmployee", "Alex", "Alexov", "alex@gmail.com", "phone"
             });
 
-            var employee = dbMock.Employees.SingleOrDefault(e => e.Email == "maila");
+            var employee = dbMock.Employees.SingleOrDefault(e => e.Email == "alex@gmail.com");
 
             // Assert
             Assert.AreEqual(1, dbMock.Employees.Count());
-            Assert.AreEqual("maila", employee.Email);
+            Assert.AreEqual("alex@gmail.com", employee.Email);
             Assert.AreEqual(returnMessage, "A new employee with name Alex Alexov was created.");
         }
     }

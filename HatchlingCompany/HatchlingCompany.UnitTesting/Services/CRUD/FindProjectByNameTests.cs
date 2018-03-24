10a9e4a662733de008e408ef7c2187973c276d16
mkovachev <martin.kovachev@outlook.com>
2018-03-24 14:57:45 +0200

    tests for FindProjectByName created

---
+++ HatchlingCompany/HatchlingCompany.UnitTesting/Services/CRUD/FindProjectByNameTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HatchlingCompany.UnitTesting.Services.CRUD
+{
+    [TestClass]
+    public class FindProjectByNameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

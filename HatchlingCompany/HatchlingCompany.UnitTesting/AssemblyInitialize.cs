3e69a5cb08474b5970bd0ed955e960c1343a429e
mkovachev <martin.kovachev@outlook.com>
2018-03-24 13:30:17 +0200

    repeating dependencies to be extracted later

--- HatchlingCompany/HatchlingCompany.UnitTesting/AssemblyInitialize.cs
+++ HatchlingCompany/HatchlingCompany.UnitTesting/AssemblyInitialize.cs
@@ -9,8 +9,17 @@ namespace HatchlingCompany.UnitTesting
         [AssemblyInitialize]
         public static void Initialize(TestContext test)
         {
+            // Automapper
             var config = new AutomapperConfig();
             config.Initialize();
+
+            //// Common
+            //var dbStub = new HatchlingCompanyDbContext(Effort.DbConnectionFactory.CreateTransient());
+            //var writerStub = new Mock<IWriter>();
+            //var mapperStub = new Mock<IMapper>();
+
+            //// Services
+            //var createEmployeeService = new CreateEmployee(dbStub, writerStub.Object, mapperStub.Object);
         }
     }
 }

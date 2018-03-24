82ac26db4c4b1442e5345803ece4ba712a0e85d1
mkovachev <martin.kovachev@outlook.com>
2018-03-24 13:33:04 +0200

    createProject tests created

---
+++ HatchlingCompany/HatchlingCompany.UnitTesting/Services/Projects/CreateProjectTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HatchlingCompany.UnitTesting.Services.Projects
+{
+    [TestClass]
+    public class CreateProjectTests
+    {
+        [TestMethod]
+        public void CreateProject_Should_Throw_ArgumentException()
+        {
+        }
+    }
+}

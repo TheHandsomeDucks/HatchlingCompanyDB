f991188abb6f10fa3b35ae290c08262d2e7ef662
mkovachev <martin.kovachev@outlook.com>
2018-03-25 19:29:47 +0300

    created

---
+++ HatchlingCompany/HatchlingCompany.UnitTesting/Services/Listing/ListProjectDetailsTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HatchlingCompany.UnitTesting.Services.Listing
+{
+    [TestClass]
+    public class ListProjectDetailsTests
+    {
+        [TestMethod]
+        public void ListProjectDetails()
+        {
+        }
+    }
+}

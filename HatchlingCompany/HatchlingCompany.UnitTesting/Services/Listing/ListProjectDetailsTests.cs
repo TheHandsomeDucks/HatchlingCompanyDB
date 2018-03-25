7524f97b2a52da51234dc431a97fef4190a6daf1
mkovachev <martin.kovachev@outlook.com>
2018-03-25 19:52:36 +0300

    fully tested

--- HatchlingCompany/HatchlingCompany.UnitTesting/Services/Listing/ListProjectDetailsTests.cs
+++ HatchlingCompany/HatchlingCompany.UnitTesting/Services/Listing/ListProjectDetailsTests.cs
@@ -1,12 +1,16 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using HatchlingCompany.Core.Common.Contracts;
+using HatchlingCompany.Core.Models;
 using HatchlingCompany.Core.Services.CRUD;
 using HatchlingCompany.Core.Services.Listing;
 using HatchlingCompany.Data;
+using HatchlingCompany.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HatchlingCompany.UnitTesting.Services.Listing
 {
@@ -79,6 +83,50 @@ namespace HatchlingCompany.UnitTesting.Services.Listing
 
             // Act && Assert
             Assert.ThrowsException<ArgumentNullException>(() => listProjectDetailsService.Execute(parameters));
+        }
+
+        [TestMethod]
+        public void ListProjectDetails_Should_Throw_ArgumentNullException_If_Project_Is_Null()
+        {
+            // Arrange
+            var parameters = new List<string>()
+            {
+                "listProjectDetails", "NoneExistingProject"
+            };
+
+            // Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => listProjectDetailsService.Execute(parameters));
+        }
+
+        [TestMethod]
+        public void ListProjectDetails_Should_Should_Find_Concrete_Project()
+        {
+            var projectToReturn = new Project
+            {
+                Name = "TestProject"
+            };
+
+            mapperStub.Setup(x => x.Map<Project>(It.IsAny<CreateProjectModel>())).Returns(projectToReturn);
+
+            createProjectService.Execute(new List<string>()
+            {
+                "createProject", "TestProject"
+            });
+
+            // Act
+            listProjectDetailsService.Execute(new List<string>()
+            {
+                "listProjectDetails", "TestProject"
+            });
+
+            var project = this.dbStub
+                        .Projects
+                        .Where(e => e.Name == "TestProject")
+                        .ProjectTo<ListProjectDetailsModel>()
+                        .SingleOrDefault();
+
+            // Assert
+            Assert.AreEqual("TestProject", project.Name);
         }
     }
 }

94f2f9dc6332910430c34f5133ed3065c4baf3ff
mkovachev <martin.kovachev@outlook.com>
2018-03-28 09:22:53 +0300

    all props added in ListEmployeeDetailsModel

--- HatchlingCompany/HatchlingCompany.Core/Models/ListEmployeeDetailsModel.cs
+++ HatchlingCompany/HatchlingCompany.Core/Models/ListEmployeeDetailsModel.cs
@@ -2,6 +2,8 @@
 using HatchlingCompany.Core.Common.Contracts;
 using HatchlingCompany.Models;
 using HatchlingCompany.Models.Common;
+using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace HatchlingCompany.Core.Models
@@ -11,11 +13,19 @@ namespace HatchlingCompany.Core.Models
 
         public string FirstName { get; set; }
 
+        public string MiddleName { get; set; }
+
         public string LastName { get; set; }
 
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+
+        public DateTime? HireDate { get; set; }
+
+        public string BankAccount { get; set; }
 
         public EmployeeStatus Status { get; set; }
 
@@ -23,6 +33,15 @@ namespace HatchlingCompany.Core.Models
 
         public string JobTitle { get; set; }
 
+        public Employee Manager { get; set; }
+
+        public Department Department { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+
+        public ICollection<Relationship> SentRelationships { get; set; }
+
+        public ICollection<Relationship> ReceivedRelationships { get; set; }
 
         public string PrintInfo()
         {
@@ -31,9 +50,17 @@ namespace HatchlingCompany.Core.Models
             sb.AppendLine($"Name: {this.FirstName} {this.LastName}");
             sb.AppendLine($"Email: {this.Email}");
             sb.AppendLine($"PhoneNumber: {this.PhoneNumber}");
+            sb.AppendLine($"Birthdate: {this.Birthdate}");
+            sb.AppendLine($"HireDate: {this.HireDate}");
+            sb.AppendLine($"BankAccount: {this.BankAccount}");
             sb.AppendLine($"Status: {this.Status}");
             sb.AppendLine($"Salary: {this.Salary}");
             sb.AppendLine($"Job Title: {this.JobTitle}");
+            sb.AppendLine($"Manager: {this.Manager}");
+            sb.AppendLine($"Department: {this.Department}");
+            sb.AppendLine(string.Join(" ", this.Projects));
+            sb.AppendLine(string.Join(" ", this.SentRelationships));
+            sb.AppendLine(string.Join(" ", this.ReceivedRelationships));
 
             return sb.ToString();
         }
